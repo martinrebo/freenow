c63712e0de5dfaf0117b24c84ea54268e3cffff8
martinrebo <martinrg33@gmail.com>
2019-03-11 12:18:55 +0100

    ready for deploy

--- src/Components/TopNavigation/Navigation.js
+++ src/Components/TopNavigation/Navigation.js
@@ -7,8 +7,10 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Code from  '@material-ui/icons/Code';
 import ClickAway from'./ClickAway';
+import Link from '@material-ui/core/Link';
 
 
 const styles = {
@@ -22,6 +24,9 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  link: {
+    margin: 5,
+  },
 };
 
 function Navigation(props) {
@@ -33,11 +38,25 @@ function Navigation(props) {
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
             <ClickAway/>
           </IconButton>
-          <Typography variant="h5" color="secondary" className={classes.grow}>
-           YOUR NOW
+          <Typography variant="h5" color="inherit" className={classes.grow}>
+           YOUR NOW (React App)
           </Typography>
 
-          <Button color="inherit">Login</Button>
+
+          <Typography>
+          <Link href="https://github.com/martinrebo/freenow" className={classes.link} color="inherit" target="_blank" rel="noopener noreferrer">
+          <Code> Github </Code>
+        
+      </Link>
+          </Typography>
+          <Typography>
+          <Link href="https://martinreboredo.com" className={classes.link} color="inherit" target="_blank" >
+          <AccountCircle> Martin Portfolio </AccountCircle>
+        
+      </Link>
+          </Typography>
+
+
         </Toolbar>
       </AppBar>
 
